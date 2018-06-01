264c57c2539e6bb19666435a617268fa0aa21089
Maggie Ghamry <maggie.ghamry@reancloud.com>
2018-06-01 16:51:04 -0400

    adds apache recipe to recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+#test recipe
+#test line 2
+
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!</h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
